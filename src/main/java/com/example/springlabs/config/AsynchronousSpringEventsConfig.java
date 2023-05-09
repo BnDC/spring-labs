0baf1b90a936ec4ef37c872c373f82947cd0b039
bufflect <bndgc@naver.com>
2023-05-10 01:16:03 +0900

    Add: AsyncSpringEventsConfig

---
+++ src/main/java/com/example/springlabs/config/AsynchronousSpringEventsConfig.java
@@ -0,0 +1,32 @@
+package com.example.springlabs.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsynchronousSpringEventsConfig {
+
+	@Bean(name = "eventExecutor")
+	public Executor eventExecutor() {
+		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+		taskExecutor.setCorePoolSize(10);
+		taskExecutor.setMaxPoolSize(30);
+		taskExecutor.setQueueCapacity(50);
+		return taskExecutor;
+	}
+
+	@Bean(name = "applicationEventMulticaster")
+	public ApplicationEventMulticaster simpleApplicationEventMulticaster() {
+		SimpleApplicationEventMulticaster simpleApplicationEventMulticaster = new SimpleApplicationEventMulticaster();
+		simpleApplicationEventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+		return simpleApplicationEventMulticaster;
+	}
+}
